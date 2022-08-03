75cf552acf58f48c4ec72d43cbe35351148e9269
lbackman <lucasbackman@gmail.com>
2022-08-03 09:18:43 +0300

    Test square printing

--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -24,4 +24,32 @@ RSpec.describe Square do
       expect(string).to eq('d1')
     end
   end
+
+  describe '#upper_lower_third' do
+    it 'returns the upper (and lower) third of the square' do
+      square = described_class.new(file: 5, rank: 2)
+      background_color = square.background_color
+      result = square.upper_lower_third
+      expect(result).to eq("     ".send("on_#{background_color}".to_sym))
+    end
+  end
+
+  describe '#middle_third' do
+    let(:piece) { double('piece', symbol: 'S') }
+    subject(:square_piece) { described_class.new(file: 5, rank: 2, piece: piece)}
+    subject(:square_empty) { described_class.new(file: 5, rank: 2, piece: nil)}
+    it 'returns the part of the square with the piece' do
+      background_color = square_piece.background_color
+      result = square_piece.middle_third
+      expected = ("  S  ").send("on_#{background_color}".to_sym)
+      expect(result).to eq(expected)
+    end
+
+    it 'returns no piece if the square is empty' do
+      background_color = square_empty.background_color
+      result = square_empty.middle_third
+      expected = ("     ").send("on_#{background_color}".to_sym)
+      expect(result).to eq(expected)
+    end
+  end
 end
