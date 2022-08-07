cc6e76994ab253c216fbcd0ae25126cb2201a597
lbackman <lucasbackman@gmail.com>
2022-08-07 17:04:40 +0300

    Add tests for Board#attacks

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe Board do
   describe '#populate_board' do
     subject(:populated_board) { described_class.new(square: SqrDbl, config: double_config) }
     let(:double_config) { Hash.new(piece: nil).merge(
-      { file: 1, rank: 2 } => { piece: [PawnDbl, 'white'] }) }
+      { file: 1, rank: 2 } => { piece: PawnDbl.new('white') }) }
     before do
       populated_board.populate_board
     end
@@ -98,6 +98,67 @@ RSpec.describe Board do
         square = change_board.current_square
         expect(square).to be(change_board.board[[7, 7]])
       end
+    end
+  end
+
+  describe '#attacks' do
+    subject(:b) { described_class.new(square: Square) }
+    before do
+      b.board[[4, 4]].piece = Pieces::Pawn.new(:white)
+      b.board[[4, 5]].piece = Pieces::Pawn.new(:black)
+      b.board[[2, 3]].piece = Pieces::Queen.new(:black)
+      b.board[[4, 2]].piece = Pieces::Knight.new(:white)
+      b.board[[1, 1]].piece = Pieces::Knight.new(:black)
+      b.board[[5, 4]].piece = Pieces::Rook.new(:white)
+      b.board[[6, 2]].piece = Pieces::King.new(:white)
+      b.board[[8, 8]].piece = Pieces::King.new(:black)
+      b.board[[8, 2]].piece = Pieces::Pawn.new(:white)
+      @wp_attacks = b.attacks(file: 4, rank: 4)
+      @bp_attacks = b.attacks(file: 4, rank: 5)
+      @bq_attacks = b.attacks(file: 2, rank: 3)
+      @wn_attacks = b.attacks(file: 4, rank: 2)
+      @bn_attacks = b.attacks(file: 1, rank: 1)
+      @wr_attacks = b.attacks(file: 5, rank: 4)
+      @wk_attacks = b.attacks(file: 6, rank: 2)
+      @bk_attacks = b.attacks(file: 8, rank: 8)
+      @p2_attacks = b.attacks(file: 8, rank: 2)
+    end
+
+    it 'white pawn on d4 has no moves' do
+      expect(@wp_attacks.size).to eq(0)
+    end
+
+    it 'black queen on b3 can move to 19 squares' do
+      expect(@bq_attacks.size).to eq(19)
+    end
+
+    it 'black queen on b3 is blocked by black pawn on d5' do
+      expect(@bq_attacks).to include([3, 4])
+      expect(@bq_attacks).to_not include([4, 5])
+    end
+
+    it 'black knight on a1 is blocked by black queen b3' do
+      expect(@bn_attacks).to eq([[3, 2]])
+    end
+
+    it 'white knight on d2 can take black queen b3' do
+      expect(@wn_attacks).to include([2, 3])
+    end
+
+    it 'black pawn on d5 can take white rook on e4 and only that square' do
+      expect(@bp_attacks).to eq [[5, 4]]
+    end
+
+    it 'white king on f2 attacks all 8 adjacent squares' do
+      expect(@wk_attacks.size).to eq(8)
+    end
+
+    it 'black king on h8 attacks 3 adjacent squares' do
+      expect(@bk_attacks.size).to eq(3)
+    end
+
+    it 'white pawn on g7 can move one or two steps forward' do
+      expect(@p2_attacks).to eq([[8, 3], [8, 4]])
     end
   end
 end
