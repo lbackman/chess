fa740e646c05b0b02a995b136372c6eb651a2a9d
lbackman <lucasbackman@gmail.com>
2022-08-24 09:23:04 +0300

    Fix rubocop offences

--- lib/game.rb
+++ lib/game.rb
@@ -1,15 +1,21 @@
+# frozen_string_literal: true
+
+# lib/game.rb
+
 require_relative 'board'
 require_relative 'human_player'
 require_relative 'computer_player'
 require_relative 'save'
 require_relative 'messages'
 
+# handles game events
 class Game
   include Messages
   include Save
-  
+
   attr_reader :board, :players
   attr_accessor :move_message, :turn_message, :check_message, :end_message, :save_message
+
   def initialize(players: [nil, nil])
     @players        = players
     @board          = set_up_board
@@ -58,7 +64,7 @@ class Game
     @turn_message = whose_turn(color)
     special_move(start, start_piece, destination, captured_piece)
     @check_message = check(color)
-    increment_ep_count(color)
+    increment_ep_count
     display
   end
 
@@ -70,7 +76,7 @@ class Game
     display
   end
 
-  def increment_ep_count(color)
+  def increment_ep_count
     board
       .all_pawns(current_player.color)
       .each { |pawn| pawn.ep_counter += 1 if pawn.times_moved == 1 }
@@ -112,12 +118,14 @@ class Game
 
   def checkmate?
     color = next_player.color
-    board.king_checked?(color) && board.squares(color).all? { |square| square[1].piece.available_moves.empty?}
+    board.king_checked?(color) &&
+      board.squares(color).all? { |square| square[1].legal_piece_moves.empty? }
   end
 
   def stalemate?
     color = next_player.color
-    !board.king_checked?(color) && board.squares(color).all? { |square| square[1].piece.available_moves.empty?}
+    !board.king_checked?(color) &&
+      board.squares(color).all? { |square| square[1].legal_piece_moves.empty? }
   end
 
   def special_move(start, start_piece, destination, captured_piece)
@@ -130,7 +138,7 @@ class Game
   end
 
   def special_pawn_moves(start, start_piece, destination, captured_piece)
-    if (destination.file - start.file) ** 2 == 1 && captured_piece.nil?
+    if (destination.file - start.file)**2 == 1 && captured_piece.nil?
       @move_message = ep_message(start, destination, start_piece)
       board.en_passant(destination, start_piece)
     elsif destination.rank == 8 || destination.rank == 1
@@ -140,29 +148,13 @@ class Game
   end
 
   def castling(start, color, destination)
-    if start.file - destination.file == 2
+    case start.file - destination.file
+    when 2
       board.castle(color, :long)
       @move_message = long_castle_message(destination.piece)
-    elsif start.file - destination.file == -2
+    when -2
       board.castle(color, :short)
       @move_message = short_castle_message(destination.piece)
     end
   end
 end
-
-
-# p1 = HumanPlayer.new(color: :white)
-# p2 = ComputerPlayer.new(color: :black)
-
-# initial_board_config = Pieces.config(:white, :black)
-# b = Board.new(config: initial_board_config)
-# b.populate_board
-# g = Game.new(players: [p1, p2], board: b)
-
-# g.play_chess
-
-# p g.choose_start
-
-# implement method that says "#{color} king in check" if in check
-# if a piece is taken it should be announced: eg. White bishop takes black pawn.
-# 
