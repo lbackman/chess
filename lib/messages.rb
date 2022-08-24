268cd19ffe0b668dcc8739367e68e0422a02fe4b
lbackman <lucasbackman@gmail.com>
2022-08-24 09:13:19 +0300

    Fix rubocop offences

--- lib/messages.rb
+++ lib/messages.rb
@@ -1,21 +1,23 @@
-#output messages
+# frozen_string_literal: true
+
+# output messages
 module Messages
   def logo
     <<~LOGO
-      ██████  ██    ██ ██████  ██    ██ 
-      ██   ██ ██    ██ ██   ██  ██  ██  
-      ██████  ██    ██ ██████    ████   
-      ██   ██ ██    ██ ██   ██    ██    
-      ██   ██  ██████  ██████     ██    
-                  
-      .d8888b.   888                                 
-      d88P  Y88b 888                                 
-      888    888 888                                 
-      888        88888b.   .d88b.  .d8888b  .d8888b  
-      888        888 "88b d8P  Y8b 88K      88K      
-      888    888 888  888 88888888 "Y8888b. "Y8888b. 
-      Y88b  d88P 888  888 Y8b.          X88      X88 
-      "Y8888P"   888  888  "Y8888   88888P'  88888P' 
+      ██████  ██    ██ ██████  ██    ██
+      ██   ██ ██    ██ ██   ██  ██  ██
+      ██████  ██    ██ ██████    ████
+      ██   ██ ██    ██ ██   ██    ██
+      ██   ██  ██████  ██████     ██
+
+      .d8888b.   888
+      d88P  Y88b 888
+      888    888 888
+      888        88888b.   .d88b.  .d8888b  .d8888b
+      888        888 "88b d8P  Y8b 88K      88K
+      888    888 888  888 88888888 "Y8888b. "Y8888b.
+      Y88b  d88P 888  888 Y8b.          X88      X88
+      "Y8888P"   888  888  "Y8888   88888P'  88888P'
     LOGO
   end
 
@@ -38,15 +40,15 @@ module Messages
   def new_game_message
     <<~NEWGAME
 
-    #{logo}
+      #{logo}
 
-    Choose game setup:
+      Choose game setup:
 
-    [1] >> Human against human
-    [2] >> Human against computer
+      [1] >> Human against human
+      [2] >> Human against computer
 
-    [E] >> Back to main menu
-    [Q] >> Exit game
+      [E] >> Back to main menu
+      [Q] >> Exit game
 
     NEWGAME
   end
@@ -77,15 +79,15 @@ module Messages
 
   def check(color)
     col = board.other_color(color)
-    board.king_checked?(col) ? "#{col.to_s.capitalize} king is checked!" : ""
+    board.king_checked?(col) ? "#{col.to_s.capitalize} king is checked!" : ''
   end
 
   def invalid_square
-    "You must choose a piece that has legal moves."
+    'You must choose a piece that has legal moves.'
   end
 
   def mate_message
-    "Checkmate!"
+    'Checkmate!'
   end
 
   def win_message(color)
@@ -93,7 +95,7 @@ module Messages
   end
 
   def stalemate_message
-    "The game ends in stalemate"
+    'The game ends in stalemate'
   end
 
   def movements_message(piece, start, destination, captured_piece)
@@ -101,16 +103,16 @@ module Messages
   end
 
   def capture_message(captured_piece)
-    captured_piece ? " and takes #{captured_piece}" : ""
+    captured_piece ? " and takes #{captured_piece}" : ''
   end
 
   def whose_turn(color)
-    "#{{white: :black, black: :white}[color].to_s.capitalize} to move"
+    "#{{ white: :black, black: :white }[color].to_s.capitalize} to move"
   end
 
   def ep_message(start, destination, pawn)
-    "#{pawn} moves from #{start} to #{destination} and takes " +
-    "#{board.ep_square(destination, pawn).piece} en passant"
+    "#{pawn} moves from #{start} to #{destination} and takes " \
+      "#{board.ep_square(destination, pawn).piece} en passant"
   end
 
   def promotion_message(message, queen)
